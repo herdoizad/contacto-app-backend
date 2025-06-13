7ed45f66505fb44428857b1a65d5a86eb51a6b68
herdoizad <david.herdoizam@gmail.com>
2025-06-12 21:38:56 -0500

    Update y delete contactos

--- routes/contacto.js
+++ routes/contacto.js
@@ -29,4 +29,29 @@ router.get('/', (req, res) => {
   res.json(contactos);
 });
 
+// Actualizar contacto por ID
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { nombre, correo, mensaje } = req.body;
+
+  const index = contactos.findIndex(m => m.id === id);
+  if (index === -1) return res.status(404).json({ error: "Contacto no encontrado" });
+
+  contactos[index].nombre = nombre;
+  contactos[index].correo = correo;
+  contactos[index].mensaje = mensaje;
+  res.json(contactos[index]);
+});
+
+// Eliminar contacto por ID
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = contactos.findIndex(m => m.id === id);
+  if (index === -1) return res.status(404).json({ error: "Contacto no encontrado" });
+
+  const contactoEliminado = contactos.splice(index, 1);
+  res.json(contactoEliminado[0]);
+});
+
 module.exports = router;
